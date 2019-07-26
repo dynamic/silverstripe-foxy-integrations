aa05b1034c86e6b7c8ae4371b09b3e223c91aaa4
Jason Irish <jsirish@users.noreply.github.com>
2019-07-26 09:36:06 -0500

    FoxyIntegrationsSetting - check if Integrations field exists (#2)

--- src/Extension/FoxyIntegrationsSetting.php
+++ src/Extension/FoxyIntegrationsSetting.php
@@ -25,25 +25,26 @@ class FoxyIntegrationsSetting extends DataExtension
      */
     public function updateCMSFields(FieldList $fields)
     {
-        $integrations = $fields->dataFieldByName('Integrations');
-        $fields->removeByName('Integrations');
+        if ($integrations = $fields->dataFieldByName('Integrations')) {
+            $fields->removeByName('Integrations');
 
-        $config = $integrations->getConfig();
-        $config
-            ->removeComponentsByType([
-                GridFieldAddExistingAutocompleter::class
-            ]);
+            $config = $integrations->getConfig();
+            $config
+                ->removeComponentsByType([
+                    GridFieldAddExistingAutocompleter::class
+                ]);
 
 
-        $fields->addFieldsToTab('Root.Integrations', array(
-            HeaderField::create('IntegrationsHeader', 'Foxy Integrations', 3),
-            LiteralField::create(
-                'IntegrationsDescip',
-                '<p>Push your Foxy.io datafeed to additional URLs for processing. This allows your datafeed to be used 
-                    by additional applications, such as 
+            $fields->addFieldsToTab('Root.Integrations', array(
+                HeaderField::create('IntegrationsHeader', 'Foxy Integrations', 3),
+                LiteralField::create(
+                    'IntegrationsDescip',
+                    '<p>Push your Foxy.io datafeed to additional URLs for processing. This allows your  
+                    datafeed to be used by additional applications, such as 
                     <a href="http://foxytools.com/orderdesk/" target="_blank">OrderDesk</a>.</p>'
-            ),
-            $integrations
-        ));
+                ),
+                $integrations
+            ));
+        }
     }
 }
